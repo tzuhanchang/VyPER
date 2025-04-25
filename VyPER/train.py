f3ad5b8d4fead9f28be02da6f56c9105825ba1ff
tzuhanchang <han.tzu.zhang@gmail.com>
2025-04-25 15:32:37 +0100

    Only save the last ckpt of the loss

--- VyPER/train.py
+++ VyPER/train.py
@@ -84,7 +84,7 @@ def Train(cfg : DictConfig) -> None:
             monitor="accuracy/dR_mean",
             save_top_k=5,
             mode="min",
-            save_last=True,
+            save_last=False,
             filename="epoch={epoch}-step={step}-dR={accuracy/dR_mean:.3f}",
             auto_insert_metric_name=False
         ),
