ce7614d9312349c73b67f658c267bf31b9a467ff
Tzu-Han Chang <57966875+tzuhanchang@users.noreply.github.com>
2025-10-24 15:58:50 +0100

    Add comments

--- VyPER/tune.py
+++ VyPER/tune.py
@@ -140,6 +140,7 @@ def Tune(cfg : DictConfig) -> None:
 if __name__ == '__main__':
     torch.set_float32_matmul_precision('medium')
 
+    # Required since PyTorch 2.6, see [#53](https://github.com/tzuhanchang/VyPER/pull/53).
     torch.serialization.add_safe_globals([torch_geometric.data.data.DataEdgeAttr])
     torch.serialization.add_safe_globals([torch_geometric.data.data.DataTensorAttr])
     torch.serialization.add_safe_globals([torch_geometric.data.storage.GlobalStorage])
